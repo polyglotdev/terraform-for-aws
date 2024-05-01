b643e4b200b00a1230a0309f720be5e49dc57d57
Dom Hallan <dom@domhallan.com>
2024-05-01 17:21:10 -0400

    Refactor provider configuration and add 'for_each' in module. - Updated AWS provider configuration to specify region as "us-east-1" - Added 'for_each' argument with environment values to the EC2 module.

--- count/module/main.tf
+++ count/module/main.tf
@@ -1,14 +1,10 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
+provider "aws" {
+  region = "us-east-1"
 }
 
 module "ec2" {
-  source = "./ec2/"
+  source   = "./ec2/"
+  for_each = toset(["dev", "test", "prod"])
 }
 
 provider "aws" {
