f13bd12f8243050a9c08f59bd9c121775b4e341b
Dom Hallan <dom@domhallan.com>
2024-05-01 17:39:14 -0400

    Add variable for number of servers
    - Added a new variable to specify the number of servers in the environment.

--- feature_switch/main.tf
+++ feature_switch/main.tf
@@ -6,6 +6,12 @@ variable "environment" {
   type = string
 }
 
+variable "number_of_servers" {
+  type        = number
+  default     = 1
+  description = "value of number of servers"
+}
+
 resource "aws_instance" "ec2" {
   ami           = "ami-032598fcc7e9d1c7a"
   instance_type = "t2.micro"
